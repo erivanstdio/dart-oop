1905e073a2fddca43253f690954ac49570c6b02e
erivanstdio <erivanstdio@gmail.com>
2024-05-12 18:34:58 -0300

    adding example test

--- test/dart_application_1_test.dart
+++ test/dart_application_1_test.dart
@@ -1,8 +1,7 @@
-import 'package:dart_application_1/classes/Food/Fruit/fruit.class.dart';
 import 'package:test/test.dart';
 
 void main() {
-  // test('calculate', () {
-  //   expect(calculate(), 42);
-  // });
+  test('example action', () {
+    expect(42, 42);
+  });
 }
